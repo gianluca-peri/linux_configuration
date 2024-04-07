c2272f889c470a1dfc0295965fc16e8b79cf917e
gianluca-peri <gianluca.peri@edu.unifi.it>
2024-04-07 11:55:51 +0200

    fixed the colors

--- bash/bashrc_configuration.sh
+++ bash/bashrc_configuration.sh
@@ -1,4 +1,8 @@
 # To make this config work add the following line to your .bashrc:
 # source /home/noumeno/linux_configuration/bash/bashrc_configuration.sh
 
+# Nice structure for the prompt
 export PS1='$(whoami)@$(hostname)[${PWD/*\//}]$ '
+
+# Fixes the annoying highlighting of folders in ls
+LS_COLORS+=':ow=01;33'
